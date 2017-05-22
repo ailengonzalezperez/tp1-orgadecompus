d578a925f47b6f31014b475cc40d6de0b05ab83e
emariotti3 <eugenia.mariotti@live.com.ar>
2017-05-21 21:52:04 -0300

    Agrego version funcional del encode en assembler

--- src/base64.h
+++ src/base64.h
@@ -11,16 +11,8 @@ decodifica los leido desde infd en base64
 y lo escribe en el file descriptor outfd*/
 int base64_decode(int infd, int outfd);
 
-//extern int concantenate_binary_to_int(unsigned char *characters);
-
-extern int issymbol(unsigned char *c, char *index);
-
-//extern int encode(int infd, int outfd, unsigned char *read_letters, int tot_read);
+extern int encode(int infd, int outfd, unsigned char *read_letters, int tot_read);
 
 extern int decode(int infd, int outfd, unsigned char *letters, int count);
-
-//extern int decode_text(int infd, int outfd);
-
-//extern int encode_text(int infd, int outfd);
 
 #endif //BASE64_H
