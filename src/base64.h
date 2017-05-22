986043b10e1cb49620e47ebfbb613c1a160f0102
Ailén González <ailengonzalezp@yahoo.com.ar>
2017-05-22 01:39:43 -0300

    base64.h completa

--- src/base64.h
+++ src/base64.h
@@ -1,15 +1,16 @@
 #ifndef BASE64_H
 #define BASE64_H
+typedef enum error { ERROR =-1, SUCCESS, INVALID_CHARACTER, IO_ERROR, MEMORY_ERROR} Error_t;
 
 /*Recibe dos file descriptors infd y outfd, 
 codifica los leido desde infd a base64 
 y lo escribe en el file descriptor outfd*/
-int base64_encode(int infd, int outfd);
+extern int base64_encode(int infd, int outfd);
 
 /*Recibe dos file descriptors infd y outfd, 
 decodifica los leido desde infd en base64 
 y lo escribe en el file descriptor outfd*/
-int base64_decode(int infd, int outfd);
+extern int base64_decode(int infd, int outfd);
 
 extern int encode(int infd, int outfd, unsigned char *read_letters, int tot_read);
 
